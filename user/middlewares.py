ced28b5bb667c5a9fd6854b06749c9376c4a6f98
libotao <2902509967@qq.com>
2020-06-19 18:14:18 +0800

    fix bug about middlewares.py

--- user/middlewares.py
+++ user/middlewares.py
@@ -4,15 +4,15 @@ from django.shortcuts import render, redirect, HttpResponse, reverse
 from rest_framework import status
 
 class AuthMD(MiddlewareMixin):  # 验证登录
-
     def process_request(self, request):  # 请求之前
+        ret={'errMsg':'', 'errCode':'', 'url':''}
         request_url = request.path_info
 
         if request_url == 'login/' or request.session.get("id"): #要登陆或已登录
             return None
         else:
-            self.ret["errMsg"] = "用户未登录"
-            self.ret["errCode"] = status.HTTP_400_BAD_REQUEST
-            self.ret["url"] = "login/"
+            ret["errMsg"] = "用户未登录"
+            ret["errCode"] = status.HTTP_400_BAD_REQUEST
+            ret["url"] = "login/"
         # 错误提示
-        return render(request, 'user/error.html', self.ret)
+        return render(request, 'user/error.html', ret)
